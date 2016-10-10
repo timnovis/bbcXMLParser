73f2988943cff5ada663d6d564688626ef16f024
Tim Novis <tim@Tanns-iMac-4.local>
2016-10-10 14:23:26 +0100

    Set a default if returnItems not requested

--- parser.php
+++ parser.php
@@ -1,13 +1,7 @@
 <?php
-
 header('Content-Type: application/json');
-
+$returnItems = isset($_REQUEST['returnItems']) ? $_REQUEST['returnItems'] : 5;
 $newsArray = json_decode(json_encode(simplexml_load_string(file_get_contents($_REQUEST['feedURL']), "SimpleXMLElement", LIBXML_NOCDATA)), true);
-
-$returnItems = $_REQUEST['returnItems'];
-
 $newsArray = array_slice($newsArray['channel']['item'], 0, $returnItems);
-
 echo json_encode(array('items' => $newsArray));
-
 ?>
